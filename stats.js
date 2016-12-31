41e8d7d5f72f5d5c0b07f0569ab652ded2e2b169
LucasBullen <bullenlucas@gmail.com>
2016-12-31 18:14:43 -0500

    Todays bar graph

--- stats.js
+++ stats.js
@@ -1,40 +1,57 @@
 window.onload = function(){
     //proof of concept that graphs work in stats view
     var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
+    var today = new Date(Date.now())
+    var siteTimes = JSON.parse(localStorage.days)[today.yyyymmdd()];
+    if(Object.keys(siteTimes).length > 0){
+        //top ten then rest
+        var timesArray = [];
+        for (var site in siteTimes) {
+            timesArray.push({'url':site,'time':(siteTimes[site]/1000)});
         }
-    });
+        timesArray = timesArray.sort(function(a, b) { return a.time < b.time ? 1 : -1; })
+        var top10 = timesArray.slice(0, 10);
+
+        timeSum = 0;
+        for (var i = 9; i < timesArray.length; i++) {
+            timeSum += timesArray[i].time;
+        }
+
+        top10.push({'url':'Other','time':timeSum});
+        var max = top10[0].time;
+        var timeLabels = [];
+        var timeData = [];
+        var timeBackgroundColor = [];
+        var timeBorderColor = [];
+        for (var i = 0; i < top10.length; i++) {
+            top10[i]
+            timeLabels.push(top10[i].url);
+            timeData.push((Math.floor(parseInt(top10[i].time))/60).toFixed(2));
+            var colour = Math.floor(((top10[i].time/max)*255));
+            timeBackgroundColor.push('rgba('+ colour +', 0, 0, 0.2)');
+            timeBorderColor.push('rgba('+ colour +', 0, 0, 1)');
+        }
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: timeLabels,//top X sites & other
+                datasets: [{
+                    label: 'Time Spent Today',
+                    data: timeData,//time (in mins)(so /60)
+                    backgroundColor: timeBackgroundColor,
+                    borderColor: timeBorderColor,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
 }
