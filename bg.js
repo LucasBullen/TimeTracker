ffe167bae6a5dae6eeb4b68fa12949794a3e3939
LucasBullen <bullenlucas@gmail.com>
2016-12-31 11:11:54 -0500

    Track site time by day, stats POC

---
+++ bg.js
@@ -0,0 +1,159 @@
+//https://developer.chrome.com/apps/idle for checking if user is idle
+
+chrome.tabs.onUpdated.addListener(function checkUpdate(tabId, changeInfo, updatedTab) {
+	if(updatedTab.status == "complete" && updatedTab.highlighted == true){
+		startTimer(updatedTab);
+	}
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+   	//Fires when the active tab in a window changes. url may not be set
+   	//startTimer(tab);
+   	chrome.tabs.get(activeInfo.tabId, function(tab) {
+	   	if (chrome.runtime.lastError) {
+        	console.log(chrome.runtime.lastError.message);
+    	}else if(tab.status == "complete"){
+	   		startTimer(tab);
+	   	}
+   	});
+
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+   	//Fired when a tab is closed.
+   	//check if tabId == current tab, if so stop timer
+   	stopTimer();
+});
+
+
+chrome.idle.setDetectionInterval(120); //seconds
+chrome.idle.onStateChanged.addListener(function(newState) {
+	console.log("state change:" + newState);
+	if(newState == "active"){
+		if(localStorage.currentURL)
+			startTimer(localStorage.currentURL);
+	}else{
+		stopTimer();
+	}
+});
+
+chrome.management.onEnabled.addListener(function(info) {
+	localStorage.startTime = Date.now();
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	    localStorage.currentURL = urlToHost(tabs[0].url);
+	});
+	console.log("START URL: " + localStorage.startTime + " at: " + localStorage.currentURL);
+})
+
+function urlToHost(url){
+	var l = document.createElement("a");
+	l.href = url;
+	var protocol = l.protocol;
+	if(protocol == "http:" || protocol == "https:")
+		return l.hostname;
+	return "null";
+}
+
+function isSameDay(date1, date2){
+	return date1.getDate() == date2.getDate()
+      && date1.getMonth() == date2.getMonth()
+      && date1.getFullYear() == date2.getFullYear();
+}
+
+function startTimer(tab){
+	if(tab.status == "complete" && urlToHost(tab.url) != localStorage.currentURL){
+		stopTimer()
+		var nowTime = new Date().toLocaleString();
+		var host = urlToHost(tab.url);
+		console.log("START URL: " + host + " at: " + nowTime);
+		//stop last timer and start new one
+		localStorage.currentURL = host;
+		localStorage.startTime = ""+Date.now();
+	}
+}
+
+function stopTimer() {
+	if(localStorage.currentURL != "null"){
+		var nowTime = new Date().toLocaleString();
+		console.log("STOP URL: " + localStorage.currentURL + " at: " + nowTime);
+		addTime(localStorage.startTime, ""+Date.now(), localStorage.currentURL)
+
+		localStorage.currentURL = "null";
+	}
+}
+
+function addTime(startStamp, endStamp, url){
+	console.log("startStamp: "+startStamp);
+	console.log("endStamp: "+endStamp);
+	var startDate = new Date(parseInt(startStamp));
+	var endDate = new Date(parseInt(endStamp));
+	while(!isSameDay(startDate,endDate)){
+		var endOfStartDate = new Date(startDate);
+		endOfStartDate.setHours(23,59,59,0);
+		console.log("startDate: "+startDate);
+		console.log("endOfStartDate: "+endOfStartDate);
+		console.log("isSameDay: "+isSameDay(startDate,endOfStartDate));
+		addTime(startDate.getTime(), endOfStartDate.getTime(), url);//for today up till midnight
+		startDate = startDate.setHours(0,0,0,0);
+		startDate = startDate.setDate(startDate.getDate()+1);//start is next day
+	}
+
+	if(!localStorage.days){
+		localStorage.days = "{}";
+	}
+	var days = JSON.parse(localStorage.days);
+	var dateString = startDate.yyyymmdd();
+
+	if(!days[dateString]){
+		days[dateString] = {};
+	}
+	if(!days[dateString][url]){
+		days[dateString][url] = 0;
+	}
+	days[dateString][url] = parseInt(days[dateString][url]) + (endStamp - startStamp);
+	localStorage.days = JSON.stringify(days);
+	console.log(localStorage);
+}
+
+Date.prototype.yyyymmdd = function() {
+  var mm = this.getMonth() + 1; // getMonth() is zero-based
+  var dd = this.getDate();
+
+  return [this.getFullYear(),
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd
+         ].join('');
+};
+
+
+function reset(){
+	chrome.storage.local.clear();
+}
+
+/*
+get days object
+if over multiple days
+	loop through days and call add
+if day is there
+	add to sites
+else
+	create day and add to sites
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ popup.js
@@ -0,0 +1,76 @@
+function yyyymmdd(date) {
+  var mm = date.getMonth() + 1; // getMonth() is zero-based
+  var dd = date.getDate();
+
+  return [date.getFullYear(),
+          (mm>9 ? '' : '0') + mm,
+          (dd>9 ? '' : '0') + dd
+         ].join('');
+};
+var timesJSON;
+window.onload = function(){
+	var urlHold = localStorage.currentURL
+	stopTimer();
+	localStorage.currentURL = urlHold;
+	localStorage.startTime = ""+Date.now();
+	document.getElementById('open-stats-button').onclick = function(){
+		chrome.tabs.create({ url: chrome.extension.getURL('stats.html')});
+	}
+	timesJSON = JSON.parse(localStorage.days)[yyyymmdd(new Date(Date.now()))];
+	drawTable();
+	window.setInterval(tick, 1000);
+}
+
+function tick(){
+	if(timesJSON){
+		drawTable();
+		timesJSON[localStorage.currentURL] += 1000;
+	}
+}
+
+function drawTable(){
+	document.getElementById('times-div').innerHTML = "";
+	var timesArray = [];
+	for (var site in timesJSON) {
+		timesArray.push({'url':site,'time':(timesJSON[site]/1000)});
+	}
+	var top10 = timesArray.sort(function(a, b) { return a.time < b.time ? 1 : -1; })
+                .slice(0, 10);
+	for (i = 0; i < top10.length; i++) {
+		var timeDiv = document.createElement('div');
+		timeDiv.innerHTML = "<p>"+top10[i].url+": "+secondsToTimeString(top10[i].time)+"</p>";
+		document.getElementById('times-div').appendChild(timeDiv);
+	}
+}
+
+function secondsToTimeString(seconds){
+	var sInYear = 31536000;
+	var sInDay = 86400;
+	var sInHour = 3600;
+	var sInMinute = 60;
+
+	var string = ""
+
+	if(seconds > sInYear){
+		string += Math.floor(seconds/sInYear) + "y ";
+		seconds = (seconds%sInYear);
+	}
+	if(seconds > sInDay){
+		string += Math.floor(seconds/sInDay) + "d ";
+		seconds = (seconds%sInDay);
+	}
+	if(seconds > sInHour){
+		string += Math.floor(seconds/sInHour) + "h ";
+		seconds = (seconds%sInHour);
+	}
+	if(seconds > sInMinute){
+		string += Math.floor(seconds/sInMinute) + "m ";
+		seconds = (seconds%sInMinute);
+	}
+	if(seconds > 1){
+		if(string != "")
+			string += "and ";
+		string += Math.floor(seconds) + "s";
+	}
+	return string;
+}
